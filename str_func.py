03012580bc8fdc435c7d32dbd1da060f1d90c189
Alexander Shinkin <sh1noprod@gmail.com>
2023-07-19 22:49:23 +0300

    added_str_title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_func(stroke):
+    """
+    возвращает все заглавные буквы строки
+    """
     return str.title(stroke)
+
+def str_title(stroke):
+    """
+    возвращает первую заглавную
+    """
+    return  str.title(stroke)
